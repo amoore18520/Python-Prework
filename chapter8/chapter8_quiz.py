6133e5ad9421caf20c4babc8312111b496273451
amoore18520 <102988266+amoore18520@users.noreply.github.com>
2022-06-06 19:49:31 -0400

    Delete chapter8 directory

--- chapter8/chapter8_quiz.py
+++
@@ -1,7 +0,0 @@
-def do_something(a_list):
-    my_string = ""
-    for element in a_list:
-        my_string += element[0]
-    return my_string
-aladdin_characters = ['Jasmine', 'Aladdin', 'Fazhal', 'Abu', 'Razoul']
-print(do_something(aladdin_characters))
