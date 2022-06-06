902c82382bee58e86be289510d17a025a68f4616
amoore18520 <102988266+amoore18520@users.noreply.github.com>
2022-06-06 19:49:59 -0400

    Delete chapter4 directory

--- chapter4/test.py
+++
@@ -1,6 +0,0 @@
-my_list = ['chicken wing', 'chicken wing', 'hot dog', 'bologna', 'chicken', 'macaroni']
-for food in my_list:
-    if food == 'chicken wing':
-        break
-    print(food, end=" ")
-
